2ebd78b4c67c53acb074294401f3879d4aea1ff2
Miguel Prata Leal <miguel.pdiogo@gmail.com>
2017-04-01 18:47:49 +0100

    Implementation and docs updates - Add new atribute into Playlist.py model - Add rest api documentation (swagger)

--- es_project_1/models/Playlist.py
+++ es_project_1/models/Playlist.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column
+from sqlalchemy import Date
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import Sequence
@@ -13,10 +14,11 @@ class Playlist(Base):
     
     id = Column(Integer, Sequence("playlist_id_seq"), primary_key=True)
     name = Column(String(100))
+    created_at = Column(Date)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship("User", back_populates="playlists")
     songs = relationship("Song", secondary = "playlist_song")
 
     def __repr__(self):
-        return "<Playlist(id = '%i', name = '%s', user_id = '%i', user_name = '%s')>"  \
-               % (self.id, self.name, self.user_id, self.user.name)
+        return "<Playlist(id = '%i', name = '%s', date = '%s', user_id = '%i', user_name = '%s')>"  \
+               % (self.id, self.name, self.created_at, self.user_id, self.user.name)
